7d97fe7974e69a42fa6eff223b839e1e90660571
BeyondBoy1 <beyondboyplays@gmail.com>
2021-06-15 10:20:39 +0530

    Removed print for bot in bot.py

--- bot.py
+++ bot.py
@@ -1,8 +1,6 @@
-<<<<<<< HEAD
 import discord
 import os
 #import keep_alive
-import math
 from discord.ext.commands import Bot
 
 bot = Bot(command_prefix='?') 
@@ -15,7 +13,7 @@ HOURS_PER_DAY = 24
 
 @bot.event
 async def on_ready():
-  print(f'{bot.user} has connected to Discord!')
+  #print(f'{bot.user} has connected to Discord!')
   await bot.change_presence(activity = discord.Game("Clash of Clans"))
   
     #final return (always round up) 
@@ -28,36 +26,3 @@ if __name__ == '__main__':
 
 #keep_alive.keep_alive()
 bot.run(token)
-
-#Test commit
-=======
-import discord
-import os
-#import keep_alive
-import math
-from discord.ext.commands import Bot
-
-bot = Bot(command_prefix='?') 
-
-token = 'TOKEN'
-
-#define constants
-MINUTES_PER_HOUR = 60
-HOURS_PER_DAY = 24
-
-@bot.event
-async def on_ready():
-  print(f'{bot.user} has connected to Discord!')
-  await bot.change_presence(activity = discord.Game("Clash of Clans"))
-  
-    #final return (always round up) 
-
-extensions = ['cogs.CommandEvents','cogs.BuilderPots','cogs.HelpInfo',]
-
-if __name__ == '__main__':
-    for ext in extensions:
-        bot.load_extension(ext)
-
-#keep_alive.keep_alive()
-bot.run(token)
->>>>>>> 02cb532d8f71fb682754a7fbadd388ac1657d747
