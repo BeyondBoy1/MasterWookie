48d0e756dfc76ac5b9b7f190971fe973d0dbb3cc
BeyondBoy1 <beyondboyplays@gmail.com>
2021-06-22 13:47:36 +0530

    Added embed auto-reaction to Interview

--- cogs/Interview.py
+++ cogs/Interview.py
@@ -11,13 +11,34 @@ class Interview(commands.Cog):
   #Sends Welcome and War Rules Images
     @commands.group(name = "interview")
     async def interview(self,ctx):
-      embed = discord.Embed(title = "General Rules",description = "React with a :thumbsup: if you've understood this")
+      embed = discord.Embed(title = "General Rules",description = "If you accept the following rules, react with a 👍 ")
       embed.set_image(url="https://media.discordapp.net/attachments/849131570618040360/854989465118441482/General_Rules.png")
-      await ctx.send(embed=embed)
+      msg1=await ctx.send(embed=embed)
+      reaction = "👍"
+      await msg1.add_reaction(emoji=reaction)
 
-      embed = discord.Embed(title = "War Rules",description = "React with a :thumbsup: if you've understood this")
+
+      embed = discord.Embed(title = "War Rules",description = "If you accept the following rules, react with a 👍")
       embed.set_image(url="https://media.discordapp.net/attachments/849131570618040360/854989415809810462/image0.png")
-      await ctx.send(embed=embed)
+      msg2=await ctx.send(embed=embed)
+      reaction = "👍"
+      await msg2.add_reaction(emoji=reaction)
+      
 
+      while True:
+          try:
+            reaction1, user1=await self.bot.wait_for("reaction_add", timeout=60)
+            if str(reaction1.emoji) == "👍" and msg1.id==reaction1.message.id:
+                try:
+                    reaction2, user2=await self.bot.wait_for("reaction_add", timeout=60)
+                    if str(reaction2.emoji) == "👍" and msg2.id==reaction2.message.id and user2.id==user1.id:
+                        Role = discord.utils.get(user2.guild.roles, name="Briefed")
+                        await user2.add_roles(Role)
+                        break 
+                except asyncio.TimeoutError:
+                    break
+          except asyncio.TimeoutError:
+              break
+      
 def setup(bot):
       bot.add_cog(Interview(bot))
