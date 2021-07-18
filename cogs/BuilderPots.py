2f699ab29d1b3037fa760efa5e4500e619ed673a
Beyond Boy <81871884+BeyondBoy1@users.noreply.github.com>
2021-07-18 23:06:03 +0530

    Update BuilderPots.py

--- cogs/BuilderPots.py
+++ cogs/BuilderPots.py
@@ -3,14 +3,16 @@ import asyncio
 import math
 import discord
 import os
+from discord import Color as c
 
 class BuilderPots(commands.Cog):
     def __init__(self,bot):
         self.bot= bot
 
-
+    
     @commands.group(name = "builderpots")
     async def builderpots(self, ctx, firstwar: float, days: float, hours: float):
+        await ctx.message.delete()
     #Potions = ((length of hero upgrade) - (time remaining in 1st war after attacks) + (Time remaining in 2nd war before attacks) - 48)/9
 
     #define constants
@@ -22,9 +24,11 @@ class BuilderPots(commands.Cog):
         if potions <=0:
             potions = 0
 
-        embed = discord.Embed(title = "Number of Potions needed for Hero Upgrades is", description = "{}".format(potions))
+        embed = discord.Embed(title = "Number of Potions needed for Hero Upgrades is", description = "{}".format(potions),color = c.blue())
+        embed.set_footer(text="Command invoked by {}".format(ctx.message.author.name))
+        
 
-        await ctx.send(embed = embed)
+        await ctx.send(embed=embed, delete_after = 60.0)
 
 
 def setup(bot):
